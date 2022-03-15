218093fc93ca49d5cea6d1fff2f92e07ca800a74
superxman1 <nickwickler1@gmail.com>
2022-03-15 18:41:25 -0400

    goes to high bar

--- src/main/java/frc/robot/commands/highBarClimb.java
+++ src/main/java/frc/robot/commands/highBarClimb.java
@@ -7,15 +7,20 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.ClimberMotor;
+import frc.robot.subsystems.ClimberSolenoid;
 
 
 public class highBarClimb extends SequentialCommandGroup {
   ClimberMotor _i = ClimberMotor.get_instance();
+  ClimberSolenoid _s = ClimberSolenoid.getInstance();
   /** Add your docs here. */
   public highBarClimb() {
-    addCommands(new moveGrippyArm(-.8, 50));
+    addCommands(new moveGrippyArm(-.8, 8), new WaitCommand(.25), new moveGrippyArm(0.25, 45) , new WaitCommand(.25), new moveGrippyArm(0.8, 60),  new WaitCommand(.5), new ToggleGrippySoliniod(),  new WaitCommand(3), new moveGrippyArm(0.8, 180),  new WaitCommand(1),  new ToggleGrippySoliniod(),  new WaitCommand(1), new moveGrippyArm(-0.8, 30), new WaitCommand(.25), new moveGrippyArm(0.25, 70), new WaitCommand(.25),  new moveGrippyArm(-0.8, 15), new WaitCommand(.25), new moveGrippyArm(0.25, 40));  
+    //instant command for solenoids
+    //,command,new command, new command, new command
   }
 
 
